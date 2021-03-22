8c09e6de5d49b78eb92a54bc9e2080555b060ee5
noahgiesel <noahgiesel@gmail.com>
2021-03-22 14:06:17 +0100

    apod nasa and spacex

--- src/Components/SpaceXdata/SpaceXdata.js
+++ src/Components/SpaceXdata/SpaceXdata.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./SpaceXdata.css";
+
 function SpaceXdata() {
+  const [apod, setApod] = useState({
+    author: "",
+    date: "",
+    explanation: "",
+    img_url: "",
+    title: "",
+  });
   const [apiData, setApiData] = useState(
     {
       name: "",
@@ -19,10 +27,12 @@ function SpaceXdata() {
   );
 
   useEffect(() => {
-    getApiData();
+    getSpaceX();
+    getApod();
+    getNasaGov();
   }, []);
 
-  const getApiData = async () => {
+  const getSpaceX = async () => {
     const api = await fetch("https://api.spacexdata.com/v4/launches/latest");
     const data = api.json();
     data
@@ -34,17 +44,58 @@ function SpaceXdata() {
       })
       .catch((e) => console.log(e));
   };
+
+  const getApod = async () => {
+    const api = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_NASA_GOV}`
+    );
+    const data = api.json();
+    data
+      .then((e) => {
+        console.log(e);
+        setApod({
+          author: e.copyright,
+          date: e.date,
+          explanation: e.explanation,
+          img_url: e.hdurl,
+          Title: e.title,
+        });
+        console.log("apod ", apod);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  const getNasaGov = async () => {
+    const api = await fetch("https://api.spacexdata.com/v4/launches/latest");
+    const data = api.json();
+    data
+      .then((e) => {
+        console.log(e);
+        setApiData({
+          name: e.name,
+        });
+      })
+      .catch((e) => console.log(e));
+  };
+
   return (
-    <div className="spaceX">
-      <h2> Latest SpaceX landing </h2>
+    <div className="Nasa-spaceX">
+      <h2>
+        Latest of <span className="nasa">NASA</span> and{" "}
+        <span className="spacex">SPACEX</span>
+      </h2>
       <div className="container">
-        <img src="" alt="lastlanding" />
-        <div className="informations">
-          <h3>Hello</h3>
-          <h3>Hello</h3>
-          <h3>Hello</h3>
-          <h3>Hello</h3>
-          <h3>Hello</h3>
+        <div className="first-section">
+          <img src={apiData.img_url} alt="apod" />
+        </div>
+        <div className="second-section">
+          <div className="informations">
+            <img src={apiData.img_url} alt="apod" />
+            <p>
+              this is the text for the image above that now is just here as a
+              placeholder
+            </p>
+          </div>
         </div>
       </div>
     </div>
